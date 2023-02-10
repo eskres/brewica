560795f7ab486dd3aa93352cdfe7ea62174b29ad
eskres <einar.skreslett@gmail.com>
2023-02-10 21:23:25 +0000

    modified nodemailer config for auto-generated ethereal credentials

--- packages/server/src/utils/nodemailer.ts
+++ packages/server/src/utils/nodemailer.ts
@@ -1,5 +1,6 @@
 import * as nodemailer from "nodemailer";
 import * as SMTPTransport from "nodemailer/lib/smtp-transport";
+import { testAccount } from "./nodemailerTestAccount"
 
 let options: SMTPTransport.Options;
 
@@ -16,12 +17,12 @@ const devOptions: SMTPTransport.Options = {
     host: process.env.SMTP_DEV_HOST,
     port: process.env.SMTP_PORT as unknown as number,
     auth: {
-        user: process.env.SMTP_DEV_USERNAME,
-        pass: process.env.SMTP_DEV_PASSWORD,
+        user: testAccount.user,
+        pass: testAccount.pass,
     }
 }
 
 
-process.env.NODE_ENV === 'production' ? options = prodOptions : options = devOptions;
+process.env.NODE_ENV === 'production' ? options = prodOptions : options = devOptions, console.log(`Ethereal credentials: ${testAccount.user} + ${testAccount.pass}`);
 
 export const transport: nodemailer.Transporter = nodemailer.createTransport(options);
