4ea9eb8e23427ff9d3992a0e3dfc829b112c43bd
eskres <einar.skreslett@gmail.com>
2023-05-18 19:04:42 +0100

    tidying up

--- packages/server/src/utils/nodemailerTestAccount.ts
+++
@@ -1,7 +0,0 @@
-import * as nodemailer from 'nodemailer';
-
-export const testAccount: nodemailer.TestAccount = await nodemailer.createTestAccount()
-.catch((err) => {
-    console.error('Failed to create a testing account. ' + err.message);
-    return process.exit(1);
-});
